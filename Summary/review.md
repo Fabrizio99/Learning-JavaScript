e89948404eeb00bf16c82ee57f5a2783b6faf8af
Fabrizio99 <avbar357159@gmail.com>
2019-06-17 00:30:00 -0500

    reviewing some topics

---
+++ Summary/review.md
@@ -0,0 +1,5 @@
+## null vs undefined
+https://exploringjs.com/impatient-js/ch_undefined-null.html#occurrences-of-undefined-and-null
+http://2ality.com/2013/10/typeof-null.html
+https://www.ajaymatharu.com/javascript-difference-between-undefined-and-null/
+https://www.todojs.com/programacion-asincrona-paso-de-continuadores-eventos-promesas-y-generadores/
---
+++ Summary/script.js
@@ -0,0 +1,29 @@
+//Entendiendo undefined
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
+// console.log(objeto)
+// var x = 1
+// var y = "Hola"
+// var z = true
+// var objeto = {nombre : 'Fabrizio'}
+// console.log(x)
+// console.log(y)
+// console.log(z)
+// console.log(objeto)
+
+// prueba que javascript no es asincrono
+// let x = function(){
+//     for (let index = 0; index < 8000; index++) {
+//         console.log('Imprimir')
+//     }
+// }
+// let presionoClick = function(){
+//     console.log("Click")
+// }
+// x()
+
+//orden de importaciones
+// var nombre = "Fabrizio"
+var nombre;
