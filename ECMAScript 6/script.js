e103b16ae4395db1cefed53964a14eca24f8c9fe
Fabrizio99 <avbar357159@gmail.com>
2019-07-23 15:41:49 -0500

    ecmascript file added

---
+++ ECMAScript 6/script.js
@@ -0,0 +1,41 @@
+// let nombre = 123
+// if(3 === 3){
+//     let nombre = "persona"
+// }
+// // let nombre = true
+// console.log(nombre)
+
+// let persona = ""
+// function funcion(){
+//     let persona = "fabrizio"
+//     return persona
+// }
+// console.log(persona)
+// console.log(funcion())
+
+// const PI = 3.1415
+// PI = 20
+
+
+// const persona = {
+//     nombre : "fabrizio",
+//     edad : 20
+// }
+// persona.nombre = "Juan"
+// console.log(persona)
+// persona.imprimir = function(){
+//     return `Soy ${this.nombre} y tengo ${this.edad} años` 
+// }
+// console.log(persona.imprimir())
+
+// const arreglo = [1,2,3]
+// console.log(arreglo)
+// arreglo.push(5)
+// console.log(arreglo)
+
+// let i=20
+for(let i = 0;i<10;i++){
+    console.log(i)
+}
+
+console.log("f",i)
