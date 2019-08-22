d216fd7bf1c3d905dc9e29c648bc9abd458ec8b4
Fabrizio99 <avbar357159@gmail.com>
2019-08-22 15:17:10 -0500

    functions improved

--- Projects/IssueTracker/main.js
+++ Projects/IssueTracker/main.js
@@ -1,6 +1,7 @@
-document.getElementById('issue-input').addEventListener('submit',(e)=>saveIssue(e))
+addEventListener('load',()=>fetchIsuues())
+document.getElementById('issue-input').addEventListener('submit',()=>saveIssue())
 
-function saveIssue(e){
+function saveIssue(){
     const issueDescription = document.getElementById('issueDescription').value
     const issueSeverity = document.getElementById('issueSeverity').value
     const issueAssigned = document.getElementById('issueAssigned').value
@@ -14,15 +15,10 @@ function saveIssue(e){
         assignedTo : issueAssigned,
         status : issueStatus
     }
-    if(localStorage.getItem('issues')==null){
-        const issues = []
-        issues.push(issue)
-        localStorage.setItem('issues',JSON.stringify(issues))
-    }else{
-        const issues = JSON.parse(localStorage.getItem('issues'))
-        issues.push(issue)
-        localStorage.setItem('issues',JSON.stringify(issues))
-    }
+
+    const issues = (localStorage.getItem('issues'))? JSON.parse(localStorage.getItem('issues')) : []
+    issues.push(issue)
+    localStorage.setItem('issues',JSON.stringify(issues))
 
     document.getElementById('issue-input').reset()
     fetchIsuues()
@@ -30,7 +26,7 @@ function saveIssue(e){
 function fetchIsuues(){
     const issues = JSON.parse(localStorage.getItem('issues'))
     const issuesList = document.getElementById('issuesList')
-    issuesList.innerHTML = '';
+    issuesList.innerHTML = ''
     if(issues){
         for (let index = 0; index < issues.length; index++) {
             let [id,desc,severity,assignedTo,status] = [issues[index].id,issues[index].description,issues[index].severity,issues[index].assignedTo,issues[index].status]
@@ -42,11 +38,28 @@ function fetchIsuues(){
             <h3>${desc}</h3>
             <p><span class="glyphicon glyphicon-time"></span>${severity}</p>
             <p><span class="glyphicon glyphicon-user"></span>${assignedTo}</p>
-            <a href="#" onclick="setStatusClosed(\"${id}\")" class="btn btn-warning">Close</a>
-            <a href="#" onclick="deleteIssue(\"${id}\")" class="btn btn-danger">Delete</a>
+            <a href="#" onclick="setStatusClosed(\'${id}\')" class="btn btn-warning">Close</a>
+            <a href="#" onclick="deleteIssue(\'${id}\')" class="btn btn-danger">Delete</a>
             </div>
             </div>`;
         }
     }
 }
-addEventListener('load',()=>fetchIsuues())
+function setStatusClosed(id){
+    const issues = JSON.parse(localStorage.getItem('issues'))
+    for (let index = 0; index < issues.length; index++) {
+        if(issues[index].id!=id)    continue
+        issues[index].status = 'Closed'
+    }
+    localStorage.setItem('issues',JSON.stringify(issues))
+    fetchIsuues()
+}
+function deleteIssue(id){
+    const issues = JSON.parse(localStorage.getItem('issues'))
+    for (let index = 0; index < issues.length; index++) {
+        if(issues[index].id!=id)    continue
+        issues.splice(index,1)
+    }
+    localStorage.setItem('issues',JSON.stringify(issues))
+    fetchIsuues()
+}
